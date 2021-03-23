92196e3035bcbdf5e80e1967a0cf869584f2d54d
Arjun-R-Nair <arjun.r.nair2002@gmail.com>
2021-03-24 03:45:37 +0530

    Made the first set of csv submission files

---
+++ file.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+data=pd.read_csv("train.csv")
+print(data.describe())
+print(data.columns)
+y = data.SalePrice
+feat = ['MSSubClass',
+    'LotArea', 'YearBuilt','OverallQual','OverallCond','1stFlrSF',
+    '2ndFlrSF','LowQualFinSF','GrLivArea','FullBath','HalfBath',
+    'BedroomAbvGr','KitchenAbvGr','TotRmsAbvGrd','Fireplaces',
+    'WoodDeckSF','OpenPorchSF','EnclosedPorch','3SsnPorch',
+    'ScreenPorch']
+    #,'YrSold','PoolArea'
+X= data[feat]
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 1)
+model=RandomForestRegressor(n_estimators=178,random_state=1)
+model.fit(X,y)
+
+test_data=pd.read_csv("test.csv")
+test_X=test_data[feat]
+test_preds=model.predict(test_X)
+
+output=pd.DataFrame({'Id': test_data.Id,'SalePrice': test_preds})
+output.to_csv('submission.csv',index=False)
--- main.py
+++ main.py
@@ -3,24 +3,32 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
-def getmar(n,train_X,val_X,train_y,val_y):
+def getmar(n,X,y,feat):
     melmodel=RandomForestRegressor(n_estimators=n,random_state=1)
-    melmodel.fit(train_X,train_y)
-    mar=mean_squared_error(val_y,melmodel.predict(val_X))
+    melmodel.fit(X,y)
+    test_data=pd.read_csv("test.csv")
+    test_X=test_data[feat]
+    mar=mean_squared_error(y,melmodel.predict(test_X))
     return(mar)
 
-data=pd.read_csv("D:/Python/train.csv")
+data=pd.read_csv("train.csv")
 print(data.describe())
 print(data.columns)
 y = data.SalePrice
-feat = ['LotArea', 'YearBuilt']
+feat = ['MSSubClass','LotArea', 'YearBuilt','OverallQual','OverallCond',
+    '1stFlrSF','2ndFlrSF','LowQualFinSF','GrLivArea','FullBath','HalfBath',
+    'BedroomAbvGr','KitchenAbvGr','TotRmsAbvGrd','Fireplaces',
+    'WoodDeckSF','OpenPorchSF','EnclosedPorch','3SsnPorch',
+    'ScreenPorch','PoolArea','YrSold']
 X=data[feat]
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 1)
+
 v=0
-mi=getmar(50,train_X,val_X,train_y,val_y)
+mi=getmar(50,X,y,feat)
 s=0
 for i in range(1,1000):
-    v=getmar(i,train_X,val_X,train_y,val_y)
+    print(i)
+    v=getmar(i,X,y,feat)
     if v<=mi:
         mi=v
         s=i
